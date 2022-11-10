16b311715bb0e2814b0db80c2b2df487c7e7989e
C2061455 <okeefesj@cardiff.ac.uk>
2022-11-10 10:28:35 +0000

    Add react bootstrap

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Users from './components/dashboard/Users';
+import Topbar from './components/reusable/Topbar/Topbar';
 
 function App() {
   return (
     <div className="App">
+      <Topbar />
       <h1>All Users</h1>
       <Users />
+
     </div>
   );
 }
