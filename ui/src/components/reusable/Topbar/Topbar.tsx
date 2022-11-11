40834f0a7f64ed56ed8060e0ecfb50a19cb48179
C2061455 <okeefesj@cardiff.ac.uk>
2022-11-11 15:13:13 +0000

    Add top bar with account settings dropdown

--- ui/src/components/reusable/Topbar/Topbar.tsx
+++ ui/src/components/reusable/Topbar/Topbar.tsx
@@ -1,33 +1,43 @@
-import React from 'react'
-import { FaUserAlt } from "react-icons/fa";
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import React, { useState } from 'react'
+import { FaUserAlt, FaCog, FaSignOutAlt } from "react-icons/fa";
+import { Container, Nav, Navbar, Dropdown, DropdownButton, Row } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css"
+
 
 function Topbar() {
+
+  // Reference - Set dropdown to appear on hover
+  // Taken from https://stackoverflow.com/a/63371122
+  const [show, setShow] = useState(false);
+
+  const showDropdown = (_e: any)=>{
+    setShow(!show);
+  }
+  const hideDropdown = (_e: any) => {
+    setShow(false);
+  }
+  // End of reference
   return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar className='py-0' id='colour-nav'>
+      <Container className="justify-content-end">
+        <Row>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Container id="hoverContainer" onMouseEnter={showDropdown} onMouseLeave={hideDropdown}>
+              <DropdownButton 
+                id="dropdown-basic-button" 
+                title={<span><FaUserAlt /> Rhys Jones </span>}
+                show={show}
+                size="sm">
+                <Dropdown.Item id='settingsDropdown' href="#"><FaCog />   SETTINGS</Dropdown.Item>
+                <Dropdown.Item id='signOutDropdown' href="#"><FaSignOutAlt />  SIGN OUT</Dropdown.Item>
+              </DropdownButton>
+            </Container>
           </Nav>
         </Navbar.Collapse>
+        </Row>
+
       </Container>
     </Navbar>
   )
