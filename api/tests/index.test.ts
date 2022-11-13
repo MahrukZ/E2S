efbe711da4eae7ee4809fb8c738cc9146b018dc5
c1986023 <Sonyw910i->
2022-11-13 21:04:45 +0000

    Create a test to test the POST request of creating an insight

--- api/tests/index.test.ts
+++ api/tests/index.test.ts
@@ -5,5 +5,13 @@ describe('GET /api/insights', () => {
     it('should get all insights', async () => {
         const res = await request(app).get("/api/insights");
         expect(res.statusCode).toBe(200);
+    }),
+    it('should create a new insight', async () => {
+        const res = await request(app)
+            .post('/api/insight')
+            .send({
+                description: 'Your carbon emissions have decreased by ...% for the ...week compared to the week...'
+            });
+        expect(res.statusCode).toEqual(201);
     });
 });
