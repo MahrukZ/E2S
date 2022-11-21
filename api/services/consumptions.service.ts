c2a9efa5dc6a9f7ee4182c0d4825bafa49d2caac
c1986023 <Sonyw910i->
2022-11-21 14:06:28 +0000

    Create getAllConsumptions and createConsumption methods in the service

---
+++ api/services/consumptions.service.ts
@@ -0,0 +1,18 @@
+import { IConsumption } from "../data/models/consumptions.model";
+import { ConsumptionRepository } from "../data/repositories/consumptions.repository";
+
+export class ConsumptionService {
+    private consumptionRepository: ConsumptionRepository;
+
+    constructor() {
+        this.consumptionRepository = new ConsumptionRepository();
+    }
+
+    async createConsumption(consumption: IConsumption) {
+        return await this.consumptionRepository.createConsumption(consumption);
+    }
+
+    async getAllConsumptions() {
+        return await this.consumptionRepository.getAllConsumptions();
+    }
+}
