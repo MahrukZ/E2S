bea0cd4576defcaab2fdfa0c0fb9e20485f24343
C2061455 <okeefesj@cardiff.ac.uk>
2022-11-22 15:37:39 +0000

    Remove unused ui dependencies

--- ui/tests/reusable/topbar/accountdropdown.test.tsx
+++ ui/tests/reusable/topbar/accountdropdown.test.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import '@testing-library/jest-dom'
-import { render, screen, cleanup, fireEvent, waitFor, getByTestId } from '@testing-library/react'
-import AccountDropdown from '../../src/components/Topbar/AccountDropdown';
+import '@testing-library/jest-dom';
+import { cleanup, render, screen } from '@testing-library/react';
+import React from 'react';
+import AccountDropdown from '../../../src/components/reusable/topbar/AccountDropdown';
 
 afterEach(() => {
     cleanup();
