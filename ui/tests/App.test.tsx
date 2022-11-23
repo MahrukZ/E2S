e3ed5324dfe9ec4c77d666941e7f3753995b613c
Jay Creasey <creaseyja@cardiff.ac.uk>
2022-11-23 12:42:43 +0000

    add jest tests for sidebar ui component

---
+++ ui/tests/App.test.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import '@testing-library/jest-dom'
+import { render, screen, cleanup, fireEvent } from '@testing-library/react'
+import App from '../src/App'
+
+
+test('should render both sidebar elements', () => {
+    // Given
+    render(<App />);
+
+    // When
+    const collapsedSideBarElement = screen.queryByTestId("collapsedSideBar");
+    const sideBarElement = screen.queryByTestId("sidebarMenu")
+
+    // Then
+    expect(collapsedSideBarElement).toBeInTheDocument();
+    expect(sideBarElement).toBeInTheDocument();
+});
+
+test('should render topbar', () => {
+    // Given
+    render(<App />);
+
+    // When
+    const topBarElement = screen.queryByTestId("topbar");
+
+    // Then
+    expect(topBarElement).toBeInTheDocument();
+});
