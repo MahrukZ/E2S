efc29da2f743fd9b6478cb563d6c11754eba3325
c2035950 <miaham@cardiff.ac.uk>
2022-11-24 11:39:35 +0000

    Add upload button with icon. Only accept file with the .csv extension

--- ui/src/components/pages/upload/Upload.tsx
+++ ui/src/components/pages/upload/Upload.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import { Form } from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
+import { FaUpload } from "react-icons/fa"
 
 function Upload() {
   return (
     <div className="container">
         <Form.Group controlId="formFile" className="mb-3">
-            <Form.Label>Upload Some Data</Form.Label>
-            <Form.Control type="file" />
+            <Form.Control type="file" accept=".csv" />
         </Form.Group>
+        <Button variant="outline-primary"><FaUpload /> Upload Data</Button>{' '}
     </div>
   );
 };
--- ui/src/components/pages/upload/UploadPage.tsx
+++ ui/src/components/pages/upload/UploadPage.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
 import Upload from './Upload';
 
-// const UploadPage: React.FunctionComponent = () => {
 function UploadPage() {
   return (
     <div className="container">
-      <h1>UploadPage</h1>
+      <h1>Upload Page</h1>
       <Upload />
     </div>
   );
