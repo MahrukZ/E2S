2bd8725598e92d1d2057081058da32604175aec9
c2035950 <miaham@cardiff.ac.uk>
2022-11-26 14:25:44 +0000

    Convert string data to float. Add site and org id to data objects

--- ui/src/components/pages/upload/UploadButton.tsx
+++ ui/src/components/pages/upload/UploadButton.tsx
@@ -4,11 +4,13 @@ import { FaUpload } from "react-icons/fa";
 import { ConsumptionsService } from "../../../services/consumptions.service";
 
 export interface IUploadData {
-  timeInterval: string,
-  heatDemand: number,
-  electricityDemand: number,
-  electricityPrice: number,
-  gasPrice: number
+  time_interval: string,
+  heat_demand: number,
+  electricity_demand: number,
+  electricity_price: number,
+  gas_price: number,
+  org_id: number,
+  site_id: number
 }
 
 interface UploadButtonProps {
@@ -16,28 +18,32 @@ interface UploadButtonProps {
 }
 
 function UploadButton({ file }: UploadButtonProps) {
-
-  const consumptionsService = new ConsumptionsService();
-
   const [csvData, setCsvData] = useState<IUploadData[]>([]);
   const [error, setError] = useState("");
 
+  const consumptionsService = new ConsumptionsService();
   const fileReader = new FileReader();
 
-  const csvFileToArray = (string:string) => {
-    const csvHeader = string.slice(0, string.indexOf("\n")).split(",");
-    const csvRows = string.slice(string.indexOf("\n") + 1).split("\n");
-
+  const csvFileToArray = (str:string) => {
+    const csvHeader = str.slice(0, str.indexOf("\n")).split(",");
+    const csvRows = str.slice(str.indexOf("\n") + 1).split("\n");
+    
     const array:IUploadData[] = csvRows.map(i => {
       const values = i.split(",");
       const obj = csvHeader.reduce((object:any, header:string, index:number) => {
-        object[header] = values[index];
+        if (header === "time_interval") {
+          object[header] = values[index];
+        } else if (header === "heat_demand" || header === "electricity_demand" || header === "electricity_price" || header === "gas_price") {
+          object[header] = parseFloat(values[index]);
+        }
+        object['site_id'] = 1;
+        object['org_id'] = 1;
         return object;
       }, {});
       return obj;
     });
     setCsvData(array);
-    console.log(array);
+    // console.log(array);
     consumptionsService.bulkCreateConsumptions(array);
   };
 
