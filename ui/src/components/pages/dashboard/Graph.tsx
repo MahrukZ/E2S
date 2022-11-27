a17403510e18af0b935e6e37c8116af29b3ae0f6
c1986023 <Sonyw910i->
2022-11-27 10:43:31 +0000

    Added useState hook to initialize the consumption state and added method setConsumption to update the state

---
+++ ui/src/components/pages/dashboard/Graph.tsx
@@ -0,0 +1,13 @@
+import { useState, useEffect, useRef } from "react";
+import { ConsumptionsService } from "../../../services/consumptions.service";
+
+function Graph(){
+  const [consumption, setConsumption] = useState({timeArray:[], electricityDemandArray:[]});
+  return (
+    <>
+    </> 
+  )
+};
+
+export default Graph;
+
