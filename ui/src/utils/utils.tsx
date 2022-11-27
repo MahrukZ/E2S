082eeb175918e16c4f862b19ee0bfa70fd83c700
c1986023 <Sonyw910i->
2022-11-27 23:45:51 +0000

    renamed variables to camelCase

--- ui/src/utils/utils.tsx
+++ ui/src/utils/utils.tsx
@@ -3,8 +3,8 @@ export function getWeekNumberByDate(date:any)
 {
     date = new Date(date);
     const firstJanuary: any = new Date(date.getFullYear(), 0, 1);
-    const dayNr: number = Math.ceil((date - firstJanuary) / (24 * 60 * 60 * 1000));
-    const weekNr: number = Math.ceil((dayNr + firstJanuary.getDay()) / 7);
-    return weekNr;
+    const dayNumber: number = Math.ceil((date - firstJanuary) / (24 * 60 * 60 * 1000));
+    const weekNumber: number = Math.ceil((dayNumber + firstJanuary.getDay()) / 7);
+    return weekNumber;
 }
 
