130e1e9c7ab196e83b0722af82a4bf99eab59908
c1986023 <Sonyw910i->
2022-11-28 12:51:31 +0000

    Add a method graphData and Create consumptions to store the returned JSON from the api call

--- ui/src/components/pages/dashboard/Graph.tsx
+++ ui/src/components/pages/dashboard/Graph.tsx
@@ -1,11 +1,18 @@
-import { useState, useEffect, useRef } from "react";
+import { useState } from "react";
+import "./Graph.css";
 import { ConsumptionsService } from "../../../services/consumptions.service";
 
-function Graph(){
-  const [consumption, setConsumption] = useState({timeArray:[], electricityDemandArray:[]});
+const consumptionsService = new ConsumptionsService();
+
+function Graph() {
+  const [consumption, setConsumption] = useState({ weekNumber: [], electricityDemand: [] });
+
+  const graphData = async () => {
+    const consumptions = await consumptionsService.getAllConsumptions();
+  };
   return (
     <>
-    </> 
+    </>
   )
 };
 
