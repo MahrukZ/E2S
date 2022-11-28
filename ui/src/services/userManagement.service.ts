da5a06c69484a4709b1d962b88560493d8cedf0e
c2035950 <miaham@cardiff.ac.uk>
2022-11-28 14:56:24 +0000

    Add service function to get all user management data

--- ui/src/services/userManagement.service.ts
+++ ui/src/services/userManagement.service.ts
@@ -1,10 +1,14 @@
-import axios from "axios"
+import axios from "axios";
 
 export class UserManagementService {
+
+    public async getAllUserManagements(): Promise<any> {
+        const response = await axios.get("/api/user-managements");
+        return await response.data;
+    }
 
     public async findUserManagementByUserId(userId: number): Promise<any> {
         const response = await axios.get(`/api/user-management/${userId}`);
         return await response.data;
     }
-
 }
