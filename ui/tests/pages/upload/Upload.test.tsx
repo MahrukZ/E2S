b62c2a192c170e73cbe1dab304dcefc822db6d6d
c2035950 <miaham@cardiff.ac.uk>
2022-11-28 18:43:42 +0000

    Remove unused import

--- ui/tests/pages/upload/Upload.test.tsx
+++ ui/tests/pages/upload/Upload.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import Upload from '../../../src/components/pages/upload/Upload'
 
 test('should render form element', () => {
