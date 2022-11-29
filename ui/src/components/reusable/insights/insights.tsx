1943489ba0c117478f1ed5fdeb610b6095d930c2
Jay Creasey <creaseyja@cardiff.ac.uk>
2022-11-29 09:42:08 +0000

    add frontend to get a site by site id

--- ui/src/components/reusable/insights/insights.tsx
+++ ui/src/components/reusable/insights/insights.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { InsightsService } from "../../../services/insights.service";
 import { SitesService } from "../../../services/sites.service";
 import { Container, Dropdown, DropdownButton } from "react-bootstrap";
+import "./insights.css";
 
 function Insights() {
   
@@ -18,15 +19,14 @@ function Insights() {
             let insightsList: String[] = [];
 
             const insightsTemplates = await insightsService.getInsights();
-            const sites = await sitesService.getSites();
+            const siteData = await sitesService.findSiteBySiteID(1);
 
+            const currentSite = siteData["data"][0]["name"];
 
             for (let i = 0; i < insightsTemplates["data"].length; i++ ) {
                 const currentInsight0: string = String(insightsTemplates["data"][i]["description"]);
 
-
-
-                const insightToAdd0 = currentInsight0.replace("[site]", "Abacws")
+                const insightToAdd0 = currentInsight0.replace("[site]", currentSite)
 
                 insightsList.push(insightToAdd0);
             }
@@ -60,7 +60,7 @@ function Insights() {
 
   return (
       <Container className="justify-content-end">
-            <div>
+            <div id="insights">
                     <h1>{insightsList[0]}</h1>
                     <h1>{insightsList[1]}</h1>
                     <h1>{insightsList[2]}</h1>
