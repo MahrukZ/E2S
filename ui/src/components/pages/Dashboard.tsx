d6677cdab916a8c865a28a647da1f4a70486835d
Jay Creasey <creaseyja@cardiff.ac.uk>
2022-11-29 09:23:50 +0000

    add insights files add initial insight placeholders

--- ui/src/components/pages/Dashboard.tsx
+++ ui/src/components/pages/Dashboard.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import Insights from "./../reusable/insights/insights"
 
 const Dashboard: React.FunctionComponent = () => {
     return (
+        <div>
         <h1 id='dashboard'>Dashboard</h1>
+        <Insights></Insights>
+        </div>
     );
 };
 
---
+++ ui/src/components/reusable/insights/insights.tsx
@@ -0,0 +1,71 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect, useState } from "react";
+import { InsightsService } from "../../../services/insights.service";
+import { SitesService } from "../../../services/sites.service";
+import { Container, Dropdown, DropdownButton } from "react-bootstrap";
+
+function Insights() {
+  
+    const [insightsList, setInsightsList] = useState<String[]>([]);
+
+    const insightsService = new InsightsService;
+    const sitesService = new SitesService;
+
+    useEffect(() => {
+
+        const getAllInsights =async () => {
+            let finalInsights: String[] = [];
+            let insightsList: String[] = [];
+
+            const insightsTemplates = await insightsService.getInsights();
+
+            for (let i = 0; i < insightsTemplates["data"].length; i++ ) {
+                const currentInsight0: string = String(insightsTemplates["data"][i]["description"]);
+
+
+
+                const insightToAdd0 = currentInsight0.replace("[site]", "Abacws")
+
+                insightsList.push(insightToAdd0);
+            }
+
+            for (let i = 0; i < insightsList.length; i++ ) {
+                const currentInsight1: string = String(insightsList[i]);
+
+                let dataForTemplate: String[] = ["8", "-7.9", "12"];
+
+                const currentData = String(dataForTemplate[i]);
+
+                const insightToAdd1 = currentInsight1.replace("[data]", currentData);
+
+                finalInsights.push(insightToAdd1);
+            }
+            
+            setInsightsList(finalInsights);
+
+        }
+        getAllInsights();
+
+        // Placeholder before consumptions service/backend implemented
+        // const getConsumptions =async () => {
+        //   const consumptionData = await consumptionsService.findConsumptionsBySite(3);
+        // }
+        // getConsumptions();
+
+      }, []);
+    
+
+
+  return (
+      <Container className="justify-content-end">
+            <div>
+                    <h1>{insightsList[0]}</h1>
+                    <h1>{insightsList[1]}</h1>
+                    <h1>{insightsList[2]}</h1>
+            </div>
+      </Container>
+
+  )
+}
+
+export default Insights
