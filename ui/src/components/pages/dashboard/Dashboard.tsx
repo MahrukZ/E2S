bd779281ac65f25f85b6b5da109ca38e9df1a761
c1986023 <Sonyw910i->
2022-11-30 09:31:15 +0000

    Imported the Graph component in the dashboard

---
+++ ui/src/components/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Graph from './Graph';
+
+const Dashboard: React.FunctionComponent = () => {
+  return (
+    <div><Graph /></div>
+  );
+};
+
+export default Dashboard;
