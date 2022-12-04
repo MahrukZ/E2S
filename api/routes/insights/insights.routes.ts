1228183983de1a4a7081f825df7a80d7a1522520
c1986023 <Sonyw910i->
2022-12-04 12:49:54 +0000

    Import the express ROUTER to indicate that this file will concentrate all the routes of the application

--- api/routes/insights/insights.routes.ts
+++ api/routes/insights/insights.routes.ts
@@ -1,33 +1,26 @@
 import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
 import { InsightController } from "./../../controllers/insights.controller";
 
-// config
-dotenv.config();
-const port = process.env.PORT || 8082;
-const app = express();
-
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
-app.use(cors());
+const router = express.Router();
 
 // controllers
 const insightController = new InsightController();
 
-//insights
-app.get("/api/insights", async (req, res) => {
+// routes
+router.get("/api/insights", async (req, res) => {
     insightController.getAllInsights(req, res);
 });
 
-app.post("/api/insight", async (req, res) => {
+router.post("/api/insight", async (req, res) => {
     insightController.createInsight(req, res);
 });
 
-app.put("/api/insight", async (req, res) => {
+router.put("/api/insight", async (req, res) => {
     insightController.updateInsight(req, res);
 });
 
-app.delete("/api/insight/:id", async (req, res) => {
+router.delete("/api/insight/:id", async (req, res) => {
     insightController.deleteInsight(req, res);
 });
+
+export default router;
--- api/routes/sites/sites.routes.ts
+++ api/routes/sites/sites.routes.ts
@@ -1,33 +1,26 @@
 import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
 import { SiteController } from "./../../controllers/sites.controller";
 
-// config
-dotenv.config();
-const port = process.env.PORT || 8082;
-const app = express();
-
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
-app.use(cors());
+const router = express.Router();
 
 // controllers
 const siteController = new SiteController();
 
-//sites
-app.get("/api/sites", async (req, res) => {
+// routes
+router.get("/api/sites", async (req, res) => {
     siteController.getAllSites(req, res);
 });
 
-app.post("/api/site", async (req, res) => {
+router.post("/api/site", async (req, res) => {
     siteController.createSite(req, res);
 });
 
-app.put("/api/site", async (req, res) => {
+router.put("/api/site", async (req, res) => {
     siteController.updateSite(req, res);
 });
 
-app.delete("/api/site/:id", async (req, res) => {
+router.delete("/api/site/:id", async (req, res) => {
     siteController.deleteSite(req, res);
 });
+
+export default router;
