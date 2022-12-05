3553ae90fb6c903cdfa9b3926bc23f9f0ed75960
c2035950 <miaham@cardiff.ac.uk>
2022-12-05 22:02:57 +0000

    Add carbon emissions graph

--- ui/src/components/reusable/graphs/DashboardGraphs.tsx
+++ ui/src/components/reusable/graphs/DashboardGraphs.tsx
@@ -1,7 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useEffect, useState } from "react";
-import { Container, Card, Col } from "react-bootstrap";
-import { SitesService } from "../../../services/sites.service";
+import { Container, Col } from "react-bootstrap";
 import { ConsumptionsService } from "../../../services/consumptions.service";
 import "./Graph.css";
 import SingleGraph from "./SingleGraph";
@@ -67,50 +66,43 @@ function DashboardGraphs() {
         name1: ""
     });
 
-    const sitesService = new SitesService;
-    const consumptionsService = new ConsumptionsService;
+    const consumptionsService = new ConsumptionsService();
 
     useEffect(() => {
-        const findAllConsumptionsBySiteAndTime = async () => {
+        const findAllConsumptionsBySiteIdAndTime = async () => {
             let electricityData = [];
             let gasData = [];
+            let emissionsData = [];
             let timeData = [];
             let electricityCostData = [];
             let gasCostData = [];
-            let emissionsData = [];
 
             const now = new Date();
             const priorDate = new Date(new Date().setDate(now.getDate() - 30));
 
-            const currentConsumptionsResponse = await consumptionsService.findAllConsumptionsBySiteAndTime(priorDate, now, currentSiteId);
+            const currentConsumptionsResponse = await consumptionsService.findAllConsumptionsBySiteIdAndTime(priorDate, now, currentSiteId);
 
             const currentConsumptionsData = currentConsumptionsResponse["data"];
 
             for (let i = 0; i < currentConsumptionsData.length; i++) {
                 const formattedElectricityDemand = parseFloat(currentConsumptionsData[i]["electricityDemand"]);
-
                 electricityData.push(formattedElectricityDemand);
 
                 const formattedGasDemand = parseFloat(currentConsumptionsData[i]["heatDemand"]);
-
                 gasData.push(formattedGasDemand);
 
-                const formattedDate = new Date(currentConsumptionsData[i]["timeInterval"]);
+                const formattedEmissions = parseFloat(currentConsumptionsData[i]["co2Emissions"]);
+                emissionsData.push(formattedEmissions);
 
+                const formattedDate = new Date(currentConsumptionsData[i]["timeInterval"]);
                 timeData.push(formattedDate);
 
                 const electricityCost = parseFloat(currentConsumptionsData[i]["electricityDemand"]) * parseFloat(currentConsumptionsData[i]["electricityPrice"]);
-
                 electricityCostData.push(electricityCost);
 
                 const gasCost = parseFloat(currentConsumptionsData[i]["heatDemand"]) * parseFloat(currentConsumptionsData[i]["gasPrice"]);
-
                 gasCostData.push(gasCost);
-
-                // const formattedEmissions = parseFloat(currentConsumptionsData[i]["co2Emissions"]);
-
-                // emissionsData.push(formattedEmissions);
-            }
+            };
 
             setElectricityGraph({
                 xData: timeData,
@@ -118,7 +110,7 @@ function DashboardGraphs() {
                 xName: "date",
                 yName: "Electricity Consumption (kWh)",
                 lineColour: "#0d609d"
-            })
+            });
 
             setGasGraph({
                 xData: timeData,
@@ -126,15 +118,15 @@ function DashboardGraphs() {
                 xName: "date",
                 yName: "Gas Consumption (kWh)",
                 lineColour: "#f15a2f"
-            })
+            });
 
-            // setEmissionsGraph({
-            //     xData: timeData,
-            //     yData: emissionsData,
-            //     xName: "date",
-            //     yName: "CO2 Emissions (INSERT UNIT HERE)",
-            //     lineColour: "#a4ba71"
-            // })
+            setEmissionsGraph({
+                xData: timeData,
+                yData: emissionsData,
+                xName: "date",
+                yName: "CO2 Emissions (kt)",
+                lineColour: "#a4ba71"
+            });
 
             setCostsGraph({
                 xData0: timeData,
@@ -146,10 +138,11 @@ function DashboardGraphs() {
                 lineColour1: "#f15a2f",
                 name0: "Electricity",
                 name1: "Gas"
-            })
+            });
         }
 
-        findAllConsumptionsBySiteAndTime();
+        findAllConsumptionsBySiteIdAndTime();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return (
@@ -160,7 +153,7 @@ function DashboardGraphs() {
 
                 <SingleGraph graphData={gasGraph} />
 
-                {/* <SingleGraph graphData={emissionsGraph} /> */}
+                <SingleGraph graphData={emissionsGraph} />
 
             </Col>
 
@@ -171,7 +164,7 @@ function DashboardGraphs() {
             </Col>
         </Container>
 
-    )
-}
+    );
+};
 
 export default DashboardGraphs;
