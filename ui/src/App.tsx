e8570a97e7af7fef1904131b51b741adeda93b43
C2061455 <okeefesj@cardiff.ac.uk>
2022-12-05 16:47:22 +0000

    add protected routes

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,16 +1,16 @@
-import React, { useEffect } from 'react';
-import './App.css';
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Axios from "axios";
-import { UsersService } from "./services/users.service";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ProtectedRoutes from "./ProtectedRoutes";
+import './App.css';
 
 import Sidebar from './components/reusable/sidebar/Sidebar';
 import Topbar from './components/reusable/topbar/Topbar';
 
-import Dashboard from './components/pages/dashboard/Dashboard';
-import Reports from './components/pages/Reports';
 import BillValidation from './components/pages/BillValidation';
 import CostForecast from './components/pages/CostForecast';
+import Dashboard from './components/pages/dashboard/Dashboard';
+import Reports from './components/pages/Reports';
 import SignIn from './components/pages/signIn/SignIn';
 
 import UploadPage from './components/pages/admin/upload/UploadPage';
@@ -28,6 +28,9 @@ const App: React.FunctionComponent = () => {
         <Topbar />
         <Sidebar />
         <Routes>
+          <Route element={<ProtectedRoutes />}>
+            
+          </Route>
           <Route path='/' element={<Dashboard />} />
           <Route path='/reports' element={<Reports />} />            
           <Route path='/billvalidation' element={<BillValidation />} />    
