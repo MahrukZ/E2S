10351926521a1b2df64c505931f88a6455f62ca9
c1986023 <Sonyw910i->
2022-12-05 20:45:00 +0000

    Modify paths to make UI test pass

--- api/tests/routes/consumptions.index.test.ts
+++ api/tests/routes/consumptions.index.test.ts
@@ -1,103 +1,104 @@
 import axios from "axios";
-import { ConsumptionController } from "../../controllers/consumptions.controller";
+import { ConsumptionController} from "../../controllers/consumptions.controller";
 import { IConsumption } from "../../data/models/consumptions.model";
 
-jest.mock("axios");
+jest.mock('axios');
 
 jest.mock('../../controllers/consumptions.controller', () => {
     const mConsumptionController = { 
         bulkCreateConsumptions: jest.fn(),
         createConsumption: jest.fn(),
-        getAllConsumptions: jest.fn()
+        getAllConsumptions: jest.fn(),
+        findAllConsumptionsBySiteIdAndTime: jest.fn(),
+        findSumOfConsumptionsBySiteIdAndTime: jest.fn()
     };
     return {
         ConsumptionController: jest.fn(() => mConsumptionController)
     };
 });
 
-describe("index", () => {
-  const controller = new ConsumptionController();
-  const mockedAxios = axios as jest.Mocked<typeof axios>;
-  const mockDateObject = new Date("2021-02-26T20:42:16.652Z");
+describe('index', () => {
+    const controller = new ConsumptionController();
+    const mockedAxios = axios as jest.Mocked<typeof axios>;
+    const mockDateObject = new Date("2021-02-26T20:42:16.652Z");
 
-  const mRequest = (body?: any, params?: any) => {
-    const req: any = {};
-    req.body = jest.fn().mockReturnValue(body || req);
-    req.params = jest.fn().mockReturnValue(params || req);
-    return req;
-  };
-  const mResponse = () => {
-    const res: any = {};
-    res.status = jest.fn().mockReturnValue(res);
-    res.json = jest.fn().mockReturnValue(res);
-    return res;
-  };
-
-  beforeEach(() => {
-    jest.clearAllMocks();
-  });
-
-  describe("GET /api/consumptions", () => {
-    const mConsumption: IConsumption[] = [
-      {
-        consumptionId: 1,
-        timeInterval: mockDateObject,
-        heatDemand: 1897,
-        electricityDemand: 1699,
-        electricityPrice: 18,
-        gasPrice: 15,
-        siteId: 1,
-        orgId: 1,
-      },
-      {
-        consumptionId: 2,
-        timeInterval: mockDateObject,
-        heatDemand: 2897,
-        electricityDemand: 2699,
-        electricityPrice: 28,
-        gasPrice: 25,
-        siteId: 2,
-        orgId: 2,
-      },
-      {
-        consumptionId: 3,
-        timeInterval: mockDateObject,
-        heatDemand: 3897,
-        electricityDemand: 3699,
-        electricityPrice: 38,
-        gasPrice: 35,
-        siteId: 3,
-        orgId: 3,
-      },
-    ];
-    const mSuccessResponse: any = {
-      message: "Success",
-      status: 200,
-      data: mConsumption,
+    const mRequest = (body?: any, params?: any) => {
+        const req: any = {};
+        req.body = jest.fn().mockReturnValue(body || req);
+        req.params = jest.fn().mockReturnValue(params || req);
+        return req;
     };
-    mockedAxios.get.mockResolvedValue(mSuccessResponse);
-    const req = mRequest();
-    const res = mResponse();
+    const mResponse = () => {
+        const res: any = {};
+        res.status = jest.fn().mockReturnValue(res);
+        res.json = jest.fn().mockReturnValue(res);
+        return res;
+    };
 
-    it("should fetch all consumptions when there is data", async () => {
-      // Given
-      const mUrl = "/api/consumptions";
-      const getSpy = jest.spyOn(controller, "getAllConsumptions");
-
-      // When
-      const result = await axios.get(mUrl);
-      await controller.getAllConsumptions(req, res);
-
-      // Then
-      expect(result).toEqual(mSuccessResponse);
-
-      expect(axios.get).toHaveBeenCalledTimes(1);
-      expect(axios.get).toHaveBeenCalledWith(mUrl);
-
-      expect(getSpy).toHaveBeenCalledTimes(1);
-      expect(getSpy).toHaveBeenCalledWith(req, res);
+    beforeEach(() => {
+        jest.clearAllMocks();
     });
-  });
+
+    describe('GET /api/consumptions', () => {
+        const mConsumption: IConsumption[] = [{
+            consumptionId: 1,
+            timeInterval: mockDateObject,
+            heatDemand: 1897,
+            electricityDemand: 1699,
+            electricityPrice: 18,
+            gasPrice: 15,
+            siteId: 1,
+            orgId: 1
+        },
+        {
+            consumptionId: 2,
+            timeInterval: mockDateObject,
+            heatDemand: 2897,
+            electricityDemand: 2699,
+            electricityPrice: 28,
+            gasPrice: 25,
+            siteId: 2,
+            orgId: 2
+        },
+        {
+            consumptionId: 3,
+            timeInterval: mockDateObject,
+            heatDemand: 3897,
+            electricityDemand: 3699,
+            electricityPrice: 38,
+            gasPrice: 35,
+            siteId: 3,
+            orgId: 3
+        }];
+        const mSuccessResponse: any = {
+            message: 'Success',
+            status: 200,
+            data: mConsumption
+        };
+        mockedAxios.get.mockResolvedValue(mSuccessResponse);
+        const req = mRequest();
+        const res = mResponse();
+
+        it('should fetch all consumptions when there is data', async () => {
+            // Given
+            const mUrl = "/api/consumptions";
+            const getSpy = jest
+                .spyOn(controller, 'getAllConsumptions');
+
+            // When
+            const result = await axios.get(mUrl);
+            await controller.getAllConsumptions(req, res);
+
+            // Then
+            expect(result).toEqual(mSuccessResponse);
+
+            expect(axios.get).toHaveBeenCalledTimes(1);
+            expect(axios.get).toHaveBeenCalledWith(mUrl);
+
+            expect(getSpy).toHaveBeenCalledTimes(1);
+            expect(getSpy).toHaveBeenCalledWith(req, res);
+        });
+    });
 
     describe('GET /api/consumption/find/:start/:end/:id', () => {
         const mConsumption: IConsumption[] = [{
@@ -243,89 +244,87 @@ describe("index", () => {
     //     const req = mRequest(mCreateBody);
     //     const res = mResponse();
 
-  //     it('should create a consumption when request body is provided', async () => {
-  //         // Given
-  //         const mUrl = "/api/consumption";
-  //         const createSpy = jest
-  //             .spyOn(controller, 'createConsumption')
-  //             .mockResolvedValue(mCreateBody);
+    //     it('should create a consumption when request body is provided', async () => {
+    //         // Given
+    //         const mUrl = "/api/consumption";
+    //         const createSpy = jest
+    //             .spyOn(controller, 'createConsumption')
+    //             .mockResolvedValue(mCreateBody);
 
-  //         // When
-  //         const result = await axios.post(mUrl);
-  //         await controller.createConsumption(req, res);
+    //         // When
+    //         const result = await axios.post(mUrl);
+    //         await controller.createConsumption(req, res);
 
-  //         // Then
-  //         expect(result).toEqual(mSuccessReponse);
+    //         // Then
+    //         expect(result).toEqual(mSuccessReponse);
 
-  //         expect(axios.post).toHaveBeenCalledTimes(1);
-  //         expect(axios.post).toHaveBeenCalledWith(mUrl);
+    //         expect(axios.post).toHaveBeenCalledTimes(1);
+    //         expect(axios.post).toHaveBeenCalledWith(mUrl);
 
-  //         expect(createSpy).toHaveBeenCalledTimes(1);
-  //         expect(createSpy).toHaveBeenCalledWith(req, res);
-  //     });
-  // });
+    //         expect(createSpy).toHaveBeenCalledTimes(1);
+    //         expect(createSpy).toHaveBeenCalledWith(req, res);
+    //     });
+    // });
 
-  describe("POST /api/consumption/bulk-create", () => {
-    const mBulkCreateBody: IConsumption[] = [
-      {
-        consumptionId: 1,
-        timeInterval: mockDateObject,
-        heatDemand: 1897,
-        electricityDemand: 1699,
-        electricityPrice: 18,
-        gasPrice: 15,
-        siteId: 1,
-        orgId: 1,
-      },
-      {
-        consumptionId: 2,
-        timeInterval: mockDateObject,
-        heatDemand: 2897,
-        electricityDemand: 2699,
-        electricityPrice: 28,
-        gasPrice: 25,
-        siteId: 2,
-        orgId: 2,
-      },
-      {
-        consumptionId: 3,
-        timeInterval: mockDateObject,
-        heatDemand: 3897,
-        electricityDemand: 3699,
-        electricityPrice: 38,
-        gasPrice: 35,
-        siteId: 3,
-        orgId: 3,
-      },
-    ];
-    const mSuccessReponse: any = {
-      message: "Created",
-      status: 201,
-      data: mBulkCreateBody,
-    };
-    mockedAxios.post.mockResolvedValue(mSuccessReponse);
-    const req = mRequest(mBulkCreateBody);
-    const res = mResponse();
+    describe('POST /api/consumption/bulk-create', () => {
+        const mBulkCreateBody: IConsumption[] = [{
+            consumptionId: 1,
+            timeInterval: mockDateObject,
+            heatDemand: 1897,
+            electricityDemand: 1699,
+            electricityPrice: 18,
+            gasPrice: 15,
+            siteId: 1,
+            orgId: 1
+        },
+        {
+            consumptionId: 2,
+            timeInterval: mockDateObject,
+            heatDemand: 2897,
+            electricityDemand: 2699,
+            electricityPrice: 28,
+            gasPrice: 25,
+            siteId: 2,
+            orgId: 2
+        },
+        {
+            consumptionId: 3,
+            timeInterval: mockDateObject,
+            heatDemand: 3897,
+            electricityDemand: 3699,
+            electricityPrice: 38,
+            gasPrice: 35,
+            siteId: 3,
+            orgId: 3
+        }];
+        const mSuccessReponse: any = {
+            message: 'Created',
+            status: 201,
+            data: mBulkCreateBody
+        };
+        mockedAxios.post.mockResolvedValue(mSuccessReponse);
+        const req = mRequest(mBulkCreateBody);
+        const res = mResponse();
 
-    it("should bulk create consumptions when request body is provided", async () => {
-      // Given
-      const mUrl = "/api/consumption/bulk-create";
-      const bulkCreateSpy = jest
-        .spyOn(controller, "bulkCreateConsumptions")
-        .mockResolvedValue(mBulkCreateBody);
+        it('should bulk create consumptions when request body is provided', async () => {
+            // Given
+            const mUrl = "/api/consumption/bulk-create";
+            const bulkCreateSpy = jest
+                .spyOn(controller, 'bulkCreateConsumptions')
+                .mockResolvedValue(mBulkCreateBody);
 
-      // When
-      const result = await axios.post(mUrl);
-      await controller.bulkCreateConsumptions(req, res);
+            // When
+            const result = await axios.post(mUrl);
+            await controller.bulkCreateConsumptions(req, res);
 
-      // Then
-      expect(result).toEqual(mSuccessReponse);
+            // Then
+            expect(result).toEqual(mSuccessReponse);
 
-      expect(axios.post).toHaveBeenCalledTimes(1);
-      expect(axios.post).toHaveBeenCalledWith(mUrl);
+            expect(axios.post).toHaveBeenCalledTimes(1);
+            expect(axios.post).toHaveBeenCalledWith(mUrl);
 
-      expect(bulkCreateSpy).toHaveBeenCalledTimes(1);
-      expect(bulkCreateSpy).toHaveBeenCalledWith(req, res);
+            expect(bulkCreateSpy).toHaveBeenCalledTimes(1);
+            expect(bulkCreateSpy).toHaveBeenCalledWith(req, res);
+        });
     });
-  });
 });
--- api/tests/routes/sitesAndUsers.index.test.ts
+++ api/tests/routes/sitesAndUsers.index.test.ts
@@ -1,134 +1,142 @@
-import axios from "axios";
-import { SitesAndUsersController } from "../../controllers/sitesAndUsers.controller";
-import { ISitesAndUser } from "../../data/models/sitesAndUsers.model";
+import axios from "axios"
+import { UserManagementController } from "../../controllers/userManagement.controller";
+import { IUserManagement } from "../../data/models/userManagement.model";
 
-jest.mock("axios");
+jest.mock('axios');
 
-jest.mock("../../controllers/sitesAndUsers.controller", () => {
-  const mSitesAndUsersController = {
-    getAllSitesAndUsers: jest.fn(),
-    findSitesAndUsersByUserId: jest.fn(),
-  };
-  return {
-    SitesAndUsersController: jest.fn(() => mSitesAndUsersController),
-  };
+jest.mock('../../controllers/userManagement.controller', () => {
+    const mUserManagementController = { 
+        getAllUserManagements: jest.fn(),
+        findUserManagementByUserId: jest.fn()
+    };
+    return {
+        UserManagementController: jest.fn(() => mUserManagementController)
+    };
 });
 
-describe("index", () => {
-  const controller = new SitesAndUsersController();
-  const mockedAxios = axios as jest.Mocked<typeof axios>;
+describe('index', () => {
+    const controller = new UserManagementController();
+    const mockedAxios = axios as jest.Mocked<typeof axios>;
 
-  const mRequest = (body?: any, params?: any) => {
-    const req: any = {};
-    req.body = jest.fn().mockReturnValue(body || req);
-    req.params = jest.fn().mockReturnValue(params || req);
-    return req;
-  };
-  const mResponse = () => {
-    const res: any = {};
-    res.status = jest.fn().mockReturnValue(res);
-    res.json = jest.fn().mockReturnValue(res);
-    return res;
-  };
-
-  beforeEach(() => {
-    jest.clearAllMocks();
-  });
-
-  describe("GET /api/sites_and_users", () => {
-    const mSitesAndUser: ISitesAndUser[] = [
-      {
-        siteId: 1,
-        name: "Abacws",
-        userId: 1,
-      },
-      {
-        siteId: 1,
-        name: "Abacws",
-        userId: 3,
-      },
-      {
-        siteId: 2,
-        name: "National Software Academy",
-        userId: 1,
-      },
-    ];
-    const mSuccessResponse: any = {
-      message: "Success",
-      status: 200,
-      data: mSitesAndUser,
+    const mRequest = (body?: any, params?: any) => {
+        const req: any = {};
+        req.body = jest.fn().mockReturnValue(body || req);
+        req.params = jest.fn().mockReturnValue(params || req);
+        return req;
     };
-    mockedAxios.get.mockResolvedValue(mSuccessResponse);
-    const req = mRequest();
-    const res = mResponse();
-
-    it("should fetch all sites and users when there is data", async () => {
-      // Given
-      const mUrl = "/api/sites_and_users";
-      const getSpy = jest.spyOn(controller, "getAllSitesAndUsers");
-
-      // When
-      const result = await axios.get(mUrl);
-      await controller.getAllSitesAndUsers(req, res);
-
-      // Then
-      expect(result).toEqual(mSuccessResponse);
-
-      expect(axios.get).toHaveBeenCalledTimes(1);
-      expect(axios.get).toHaveBeenCalledWith(mUrl);
-
-      expect(getSpy).toHaveBeenCalledTimes(1);
-      expect(getSpy).toHaveBeenCalledWith(req, res);
-    });
-  });
-
-  describe("GET /api/sites_and_users/:id", () => {
-    const mSitesAndUser: ISitesAndUser[] = [
-      {
-        siteId: 1,
-        name: "Abacws",
-        userId: 1,
-      },
-      {
-        siteId: 1,
-        name: "Abacws",
-        userId: 3,
-      },
-      {
-        siteId: 2,
-        name: "National Software Academy",
-        userId: 1,
-      },
-    ];
-    const mSuccessResponse1: any = {
-      message: "Success",
-      status: 200,
-      data: mSitesAndUser,
+    const mResponse = () => {
+        const res: any = {};
+        res.status = jest.fn().mockReturnValue(res);
+        res.json = jest.fn().mockReturnValue(res);
+        return res;
     };
-    const mFetchParams: number = 1;
-    mockedAxios.get.mockResolvedValue(mSuccessResponse1);
-    const req = mRequest("", mFetchParams);
-    const res = mResponse();
 
-    it("should find sites and users when request params are provided", async () => {
-      // Given
-      const mUrl = `/api/sites_and_users/${mFetchParams}`;
-      const getSpy = jest
-        .spyOn(controller, "findSitesAndUsersByUserId")
-        .mockResolvedValue(mFetchParams);
-
-      // When
-      const result = await axios.get(mUrl);
-      await controller.findSitesAndUsersByUserId(req, res);
-
-      // Then
-      expect(result).toEqual(mSuccessResponse1);
-
-      expect(axios.get).toHaveBeenCalledTimes(1);
-      expect(axios.get).toHaveBeenCalledWith(mUrl);
-
-      expect(getSpy).toHaveBeenCalledTimes(1);
-      expect(getSpy).toHaveBeenCalledWith(req, res);
+    beforeEach(() => {
+        jest.clearAllMocks();
     });
-  });
+
+    // Commented out since you can only mock one axios get request
+    // describe('GET /api/user-managements', () => {
+    //     const mUserManagements: IUserManagement[] = [
+    //         {
+    //             userId: 1,
+    //             firstName: "Rhys",
+    //             lastName: "Jones",
+    //             email: "rhys.jones@cardiff.ac.uk",
+    //             organisation: "Cardiff University",
+    //             noSitesManaged: 3,
+    //             role: "facility energy manager"
+    //         },
+    //         {
+    //             userId: 2,
+    //             firstName: "Johnny",
+    //             lastName: "Bravo",
+    //             email: "johnny.bravo@nhs.gov.uk",
+    //             organisation: "National Health Service",
+    //             noSitesManaged: 7,
+    //             role: "director of estates"
+    //         },
+    //         {
+    //             userId: 3,
+    //             firstName: "Cai",
+    //             lastName: "Robert",
+    //             email: "cairobert@e2s.co.uk",
+    //             organisation: "Empowering Energy Solutions",
+    //             noSitesManaged: 0,
+    //             role: "administrator"
+    //         }
+    //     ];
+    //     const mSuccessResponse: any = {
+    //         message: 'Success',
+    //         status: 200,
+    //         data: mUserManagements
+    //     };
+    //     const mFetchParams: number = 1;
+    //     mockedAxios.get.mockResolvedValue(mSuccessResponse);
+    //     const req = mRequest('', mFetchParams);
+    //     const res = mResponse();
+
+    //     it('should find user managements when request params are provided', async () => {
+    //         // Given
+    //         const mUrl = `/api/user-managements`;
+    //         const getSpy = jest
+    //             .spyOn(controller, 'getAllUserManagements')
+    //             .mockResolvedValue(mFetchParams);
+
+    //         // When
+    //         const result = await axios.get(mUrl);
+    //         await controller.getAllUserManagements(req, res);
+
+    //         // Then
+    //         expect(result).toEqual(mSuccessResponse);
+
+    //         expect(axios.get).toHaveBeenCalledTimes(1);
+    //         expect(axios.get).toHaveBeenCalledWith(mUrl);
+
+    //         expect(getSpy).toHaveBeenCalledTimes(1);
+    //         expect(getSpy).toHaveBeenCalledWith(req, res);
+    //     });
+    // });
+
+    describe('GET /api/user-management/:id', () => {
+        const mUserManagement: IUserManagement = {
+            userId: 1,
+            firstName: "Rhys",
+            lastName: "Jones",
+            email: "rhys.jones@cardiff.ac.uk",
+            organisation: "Cardiff University",
+            noSitesManaged: 3,
+            role: "facility energy manager"
+        };
+        const mSuccessResponse: any = {
+            message: 'Success',
+            status: 200,
+            data: mUserManagement
+        };
+        const mFetchParams: number = 1;
+        mockedAxios.get.mockResolvedValue(mSuccessResponse);
+        const req = mRequest('', mFetchParams);
+        const res = mResponse();
+
+        it('should find user managements when request params are provided', async () => {
+            // Given
+            const mUrl = `/api/user-management/${mFetchParams}`;
+            const getSpy = jest
+                .spyOn(controller, 'findUserManagementByUserId')
+                .mockResolvedValue(mFetchParams);
+
+            // When
+            const result = await axios.get(mUrl);
+            await controller.findUserManagementByUserId(req, res);
+
+            // Then
+            expect(result).toEqual(mSuccessResponse);
+
+            expect(axios.get).toHaveBeenCalledTimes(1);
+            expect(axios.get).toHaveBeenCalledWith(mUrl);
+
+            expect(getSpy).toHaveBeenCalledTimes(1);
+            expect(getSpy).toHaveBeenCalledWith(req, res);
+        });
+    });
 });
