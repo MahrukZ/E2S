a5942780a55ce276c96d80b43fa4b3b1148dadce
Jay Creasey <creaseyja@cardiff.ac.uk>
2022-12-05 15:16:50 +0000

    fix ui test fail

--- ui/src/services/sites.service.ts
+++ ui/src/services/sites.service.ts
@@ -2,10 +2,10 @@ import axios from "axios"
 
 export class SitesService {
 
-    // public async getSites(): Promise<any> {
-    //     const response = await axios.get('/api/sites');
-    //     return await response.data;
-    // }
+    public async getSites(): Promise<any> {
+        const response = await axios.get('/api/sites');
+        return await response.data;
+    }
 
     public async findSiteById(siteId: number): Promise<any> {
         const response = await axios.get(`api/site/${siteId}`);
