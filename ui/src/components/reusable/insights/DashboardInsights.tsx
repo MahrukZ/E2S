a939c7e845cf41fde653cbc4ab30d7731f0c2d86
c2035950 <miaham@cardiff.ac.uk>
2022-12-07 18:58:41 +0000

    Add co2 emissions to DashboardInsights

--- ui/src/components/reusable/insights/DashboardInsights.tsx
+++ ui/src/components/reusable/insights/DashboardInsights.tsx
@@ -31,10 +31,18 @@ function DashboardInsights() {
         percentage: "",
         isPositive: false
     });
+
+    const [emissionsInsight, setEmissionsInsight] = useState<IInsightData>({
+      title: "",
+      insightList: [],
+      percentage: "",
+      isPositive: false,
+    });
     
     // Sets the colour based on whether the change or positive or negative
     const [isElectricityPositive, setIsElectricityPositive] = useState(false);
     const [isGasPositive, setIsGasPositive] = useState(false);
+    const [isEmissionsPositive, setIsEmissionsPositive] = useState(false);
     const [isCostPositive, setIsCostPositive] = useState(false);
 
     // Initialize services
@@ -46,6 +54,7 @@ function DashboardInsights() {
         let costsInsightsList: String[] = [];
         let electricityInsightsList: String[] = [];
         let gasInsightsList: String[] = [];
+        let emissionsInsightsList: String[] = [];
 
         // This will only work with 3 insight templates in the database
         const getAllInsights = async () => {
@@ -59,7 +68,7 @@ function DashboardInsights() {
             const currentSite = siteData["data"][0]["name"];
 
             // Replace [site] in the template with site name
-            for (let i = 0; i < 3; i++ ) {
+            for (let i = 0; i < 4; i++ ) {
                 const currentInsight0: string = String(insightsTemplates["data"][i]["description"]);
 
                 const insightToAdd0 = currentInsight0.replace("[site]", currentSite)
@@ -79,91 +88,133 @@ function DashboardInsights() {
             costsInsightsList = [finalInsights[0], finalInsights[1]];
             electricityInsightsList = [finalInsights[2], finalInsights[3]];
             gasInsightsList = [finalInsights[4], finalInsights[5]];
+            emissionsInsightsList = [finalInsights[6], finalInsights[7]];
         }
         getAllInsights();
 
         const findSumOfConsumptionsBySiteIdAndTime = async () => {
-            let costPct: String = "";
-            let electricityPct: String = "";
-            let gasPct: String = "";
+          let costPct: String = "";
+          let electricityPct: String = "";
+          let gasPct: String = "";
+          let emissionsPct: String = "";
 
-            const now = new Date();
-            const priorDate = new Date(new Date().setDate(now.getDate() - 7));
-            const priorPriorDate = new Date(new Date().setDate(now.getDate() - 14));
+          const now = new Date();
+          const priorDate = new Date(new Date().setDate(now.getDate() - 7));
+          const priorPriorDate = new Date(
+            new Date().setDate(now.getDate() - 14)
+          );
 
-            const currentConsumptionsResponse = await consumptionsService.findSumOfConsumptionsBySiteIdAndTime(priorDate, now, currentSiteId);
-            const previousConsumptionsResponse = await consumptionsService.findSumOfConsumptionsBySiteIdAndTime(priorPriorDate, priorDate, currentSiteId);
-            
-            const currentConsumptionsData = currentConsumptionsResponse["data"];
-            const previousConsumptionsData = previousConsumptionsResponse["data"];
-
-            const totalCurrentElectricityDemand = currentConsumptionsData[0];
-            const totalCurrentGasDemand = currentConsumptionsData[1];
-            const totalCurrentCosts = currentConsumptionsData[2];
-
-            const totalPreviousElectricityDemand = previousConsumptionsData[0];
-            const totalPreviousGasDemand = previousConsumptionsData[1];
-            const totalPreviousCosts = previousConsumptionsData[2];
-
-            // calculate percentage
-            const electricityPercentage = Math.round(
-                (totalCurrentElectricityDemand - totalPreviousElectricityDemand) / totalPreviousElectricityDemand * 100
+          const currentConsumptionsResponse =
+            await consumptionsService.findSumOfConsumptionsBySiteIdAndTime(
+              priorDate,
+              now,
+              currentSiteId
             );
-            const gasPercentage = Math.round(
-                (totalCurrentGasDemand - totalPreviousGasDemand) / totalPreviousGasDemand * 100
-            );
-            const costPercentage = Math.round(
-                (totalCurrentCosts - totalPreviousCosts) / totalPreviousCosts * 100
+          const previousConsumptionsResponse =
+            await consumptionsService.findSumOfConsumptionsBySiteIdAndTime(
+              priorPriorDate,
+              priorDate,
+              currentSiteId
             );
 
-            // add plus or minus symbol and change colour
-            if (electricityPercentage <= 0) {
-                const stringElectricityPercentage = String(electricityPercentage);
-                electricityPct = stringElectricityPercentage;
-            } else {
-                const stringElectricityPercentage = "+" + String(electricityPercentage);
-                electricityPct = stringElectricityPercentage;
-                setIsElectricityPositive(true);
-            };
+          const currentConsumptionsData = currentConsumptionsResponse["data"];
+          const previousConsumptionsData = previousConsumptionsResponse["data"];
 
-            if (gasPercentage <= 0) {
-                const stringGasPercentage = String(gasPercentage);
-                gasPct = stringGasPercentage;
-            } else {
-                const stringGasPercentage = "+" + String(gasPercentage);
-                gasPct = stringGasPercentage;
-                setIsGasPositive(true);
-            };
+          const totalCurrentElectricityDemand = currentConsumptionsData[0];
+          const totalCurrentGasDemand = currentConsumptionsData[1];
+          const totalCurrentEmissions = currentConsumptionsData[2];
+          const totalCurrentCosts = currentConsumptionsData[3];
 
-            if (costPercentage <= 0) {
-                const stringCostPercentage = String(costPercentage);
-                costPct = stringCostPercentage;
-            } else {
-                const stringCostPercentage = "+" + String(costPercentage);
-                costPct = stringCostPercentage;
-                setIsCostPositive(true);
-            };
+          const totalPreviousElectricityDemand = previousConsumptionsData[0];
+          const totalPreviousGasDemand = previousConsumptionsData[1];
+          const totalPreviousEmissions = previousConsumptionsData[2];
+          const totalPreviousCosts = previousConsumptionsData[3];
 
-            setCostsInsight({
-                title: "Total Costs",
-                insightList: costsInsightsList,
-                percentage: costPct,
-                isPositive: isCostPositive
-            });
+          // calculate percentage
+          const electricityPercentage = Math.round(
+            ((totalCurrentElectricityDemand - totalPreviousElectricityDemand) /
+              totalPreviousElectricityDemand) *
+              100
+          );
+          const gasPercentage = Math.round(
+            ((totalCurrentGasDemand - totalPreviousGasDemand) /
+              totalPreviousGasDemand) *
+              100
+          );
+          const emissionsPercentage = Math.round(
+            ((totalCurrentEmissions - totalPreviousEmissions) /
+              totalPreviousEmissions) *
+              100
+          );
+          const costPercentage = Math.round(
+            ((totalCurrentCosts - totalPreviousCosts) / totalPreviousCosts) *
+              100
+          );
 
-            setElectricityInsight({
-                title: "Electricity Insight",
-                insightList: electricityInsightsList,
-                percentage: electricityPct,
-                isPositive: isElectricityPositive
-            });
+          // add plus or minus symbol and change colour
+          if (electricityPercentage <= 0) {
+            const stringElectricityPercentage = String(electricityPercentage);
+            electricityPct = stringElectricityPercentage;
+          } else {
+            const stringElectricityPercentage = "+" + String(electricityPercentage);
+            electricityPct = stringElectricityPercentage;
+            setIsElectricityPositive(true);
+          };
 
-            setGasInsight({
-                title: "Gas Insight",
-                insightList: gasInsightsList,
-                percentage: gasPct,
-                isPositive: isGasPositive
-            });
+          if (gasPercentage <= 0) {
+            const stringGasPercentage = String(gasPercentage);
+            gasPct = stringGasPercentage;
+          } else {
+            const stringGasPercentage = "+" + String(gasPercentage);
+            gasPct = stringGasPercentage;
+            setIsGasPositive(true);
+          };
+
+          if (emissionsPercentage <= 0) {
+            const stringEmissionsPercentage = String(emissionsPercentage);
+            emissionsPct = stringEmissionsPercentage;
+          } else {
+            const stringEmissionsPercentage = "+" + String(emissionsPercentage);
+            emissionsPct = stringEmissionsPercentage;
+            setIsEmissionsPositive(true);
+          };
+
+          if (costPercentage <= 0) {
+            const stringCostPercentage = String(costPercentage);
+            costPct = stringCostPercentage;
+          } else {
+            const stringCostPercentage = "+" + String(costPercentage);
+            costPct = stringCostPercentage;
+            setIsCostPositive(true);
+          };
+
+          setCostsInsight({
+            title: "Total Costs",
+            insightList: costsInsightsList,
+            percentage: costPct,
+            isPositive: isCostPositive,
+          });
+
+          setElectricityInsight({
+            title: "Electricity Insight",
+            insightList: electricityInsightsList,
+            percentage: electricityPct,
+            isPositive: isElectricityPositive,
+          });
+
+          setGasInsight({
+            title: "Gas Insight",
+            insightList: gasInsightsList,
+            percentage: gasPct,
+            isPositive: isGasPositive,
+          });
+
+          setEmissionsInsight({
+            title: "CO2 Emissions Insight",
+            insightList: emissionsInsightsList,
+            percentage: emissionsPct,
+            isPositive: isEmissionsPositive,
+          });
         }
         findSumOfConsumptionsBySiteIdAndTime();
 
@@ -171,17 +222,17 @@ function DashboardInsights() {
     }, []);
     
     return (
-        <Container className="justify-content-end">
-            <Col className="d-flex insightsCol">
+      <Container className="justify-content-end">
+        <Col className="d-flex insightsCol">
+          <Insight insightData={costsInsight} />
 
-                <Insight insightData={costsInsight} />
+          <Insight insightData={electricityInsight} />
 
-                <Insight insightData={electricityInsight} />
+          <Insight insightData={gasInsight} />
 
-                <Insight insightData={gasInsight} />
-
-            </Col>
-        </Container>
+          <Insight insightData={emissionsInsight} />
+        </Col>
+      </Container>
     );
 };
 
