afbe08840dca6565b558be7c8f0d9b6ee54a00b6
Jay Creasey <creaseyja@cardiff.ac.uk>
2022-12-07 15:07:06 +0000

    Add frontend for sums card

--- ui/src/components/reusable/datePicker/DatePicker.tsx
+++ ui/src/components/reusable/datePicker/DatePicker.tsx
@@ -1,3 +1,4 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import { addDays } from "date-fns";
 import { Range, DateRangePicker } from "react-date-range";
 import { useState, useEffect } from "react";
@@ -8,51 +9,49 @@ import "./DatePicker.css";
 import Sums from "../sums/Sums";
 
 export interface ISums {
-  betweenDates: Range[];
+    betweenDates: Range[];
 }
 
 function DatePicker() {
-  const [state, setState] = useState<Range[]>([
-    {
-      startDate: new Date(),
-      endDate: addDays(new Date(), 7),
-      key: "selection",
-    },
-  ]);
+    const [state, setState] = useState<Range[]>([
+        {
+            startDate: new Date(),
+            endDate: addDays(new Date(), 7),
+            key: "selection",
+        },
+    ]);
 
-  const [betweenDates, setBetweenDates] = useState<ISums>({
-    betweenDates: state,
-  });
+    const [betweenDates, setBetweenDates] = useState<ISums>({
+        betweenDates: state,
+    });
 
-  useEffect(() => {
-    const setTheBetweenDates = async () => {
-      setBetweenDates({ betweenDates: state });
-    };
+    useEffect(() => {
+        const setTheBetweenDates = async () => {
+            setBetweenDates({ betweenDates: state });
+        };
+        setTheBetweenDates();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
-    console.log(state);
-    setTheBetweenDates();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  return (
-    <Col className="d-flex calendarContainer">
-      <Card className="flex-fill datePickerCard">
-        <DateRangePicker
-          onChange={(item) => {
-            setState([item.selection]);
-            setBetweenDates({ betweenDates: [item.selection] });
-          }}
-          moveRangeOnFirstSelection={false}
-          months={1}
-          ranges={state}
-          direction="horizontal"
-          preventSnapRefocus={true}
-          calendarFocus="backwards"
-        />
-      </Card>
-        <Sums betweenDates={betweenDates} />
-    </Col>
-  );
+    return (
+        <Col className="d-flex calendarContainer justify-content-center">
+            <Card className="flex-fill datePickerCard">
+                <DateRangePicker
+                    onChange={(item) => {
+                        setState([item.selection]);
+                        setBetweenDates({ betweenDates: [item.selection] });
+                    }}
+                    moveRangeOnFirstSelection={false}
+                    months={1}
+                    ranges={state}
+                    direction="horizontal"
+                    preventSnapRefocus={true}
+                    calendarFocus="backwards"
+                />
+            </Card>
+            <Sums betweenDates={betweenDates} />
+        </Col>
+    );
 }
 
 export default DatePicker;
--- ui/src/components/reusable/sums/Sums.tsx
+++ ui/src/components/reusable/sums/Sums.tsx
@@ -1,22 +1,111 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Card } from "react-bootstrap";
+import { useEffect, useState } from "react";
 import { ISums } from "./../datePicker/DatePicker";
 import "../datePicker/DatePicker.css";
+import { ConsumptionsService } from "../../../services/consumptions.service";
+import { SitesService } from "../../../services/sites.service";
 
 interface ISumsProps {
-  betweenDates: ISums;
+    betweenDates: ISums;
 }
 
 function Sums({ betweenDates }: ISumsProps) {
-  console.log(betweenDates);
-  return (
-    <Card className="flex-fill datePickerCard">
-      <Card.Title>Total Costs</Card.Title>
-      <Card.Body>
-        <b>hi</b>
-      </Card.Body>
-    </Card>
-  );
+    const currentSiteId = 1;
+
+    const [consumptionsList, setConsumptionsList] = useState<String[]>([]);
+    const [siteName, setSiteName] = useState<String>();
+    const [dateRange, setDateRange] = useState<String[]>([]);
+
+    const consumptionsService = new ConsumptionsService();
+    const sitesService = new SitesService();
+
+    console.log(betweenDates);
+
+    useEffect(() => {
+        const findSumOfConsumptionsBySiteIdAndTime = async () => {
+            let finalConsumptions: String[] = [];
+
+            const now = new Date();
+            const priorDate = new Date(new Date().setDate(now.getDate() - 7));
+
+            const siteData = await sitesService.findSiteById(currentSiteId);
+
+            const currentSite = siteData["data"][0]["name"];
+
+            const consumptionsResponse =
+                await consumptionsService.findSumOfConsumptionsBySiteIdAndTime(
+                    priorDate,
+                    now,
+                    currentSiteId
+                );
+
+            const consumptionsData = consumptionsResponse["data"];
+
+            const stringElectricityDemand = Math.round(
+                consumptionsData[0]
+            ).toLocaleString();
+
+            const stringGasDemand = Math.round(
+                consumptionsData[1]
+            ).toLocaleString();
+            const stringEmissions = Math.round(
+                consumptionsData[2]
+            ).toLocaleString();
+            const stringTotalCosts = Math.round(
+                consumptionsData[3]
+            ).toLocaleString();
+            const stringElectricityCosts = Math.round(
+                consumptionsData[4]
+            ).toLocaleString();
+            const stringGasCosts = Math.round(
+                consumptionsData[5]
+            ).toLocaleString();
+
+            const priorMonth = priorDate.getMonth() + 1;
+            const nowMonth = now.getMonth() + 1;
+
+            const from =
+                priorDate.getDate() +
+                "-" +
+                priorMonth +
+                "-" +
+                priorDate.getFullYear();
+            const to = now.getDate() + "-" + nowMonth + "-" + now.getFullYear();
+
+            finalConsumptions.push(
+                stringElectricityDemand,
+                stringGasDemand,
+                stringEmissions,
+                stringTotalCosts,
+                stringElectricityCosts,
+                stringGasCosts
+            );
+
+            setDateRange([from, to]);
+            setConsumptionsList(finalConsumptions);
+            setSiteName(currentSite);
+        };
+        findSumOfConsumptionsBySiteIdAndTime();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    console.log(betweenDates);
+    return (
+        <Card className="flex-fill datePickerCard">
+            <Card.Title>
+                {siteName} between {dateRange[0]} and {dateRange[1]}
+            </Card.Title>
+            <Card.Body>
+                <li>Electricity Demand: {consumptionsList[0]} kWh</li>
+                <li>Gas Demand: {consumptionsList[1]} kWh</li>
+                <li>Carbon Emissions: {consumptionsList[2]} UNIT</li>
+                <li>Electricity Costs: £ {consumptionsList[4]}</li>
+                <li>Gas Costs: £ {consumptionsList[5]}</li>
+                <li>Total Costs: £ {consumptionsList[3]}</li>
+            </Card.Body>
+        </Card>
+    );
 }
 
 export default Sums;
