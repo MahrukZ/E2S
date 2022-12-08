5f2b440253c156950b72fa25a4218564ad0c55ef
C2061455 <okeefesj@cardiff.ac.uk>
2022-12-08 18:36:02 +0000

    test commit as two branches were created

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -16,39 +16,45 @@ import AdminRoutes from "../src/routes/AdminRoutes";
 import ProtectedRoutes from "../src/routes/ProtectedRoutes";
 import Dashboard from "./components/pages/Dashboard";
 
-
 import UploadPage from "./components/pages/admin/upload/UploadPage";
 import UserManagementPage from "./components/pages/admin/userManagement/UserManagementPage";
 
 const App: React.FunctionComponent = () => {
-  Axios.defaults.withCredentials = true;
-  //Defines the paths of each page
-  //This file should only have the topbar and sidebar
-  return (
-    <>
-      <Router>
-        <Topbar />
-        <Sidebar />
-        <Routes>
-          <Route element={<ProtectedRoutes />}>
-            <Route path="/reports" element={<Reports />} />
-            <Route path="/billvalidation" element={<BillValidation />} />
-            <Route path="/costforecast" element={<CostForecast />} />
-            <Route path="/" element={<Dashboard />} />
-          </Route>
-          {/* admin routes */}
-          <Route element={<AdminRoutes />}>
-            <Route path="/admin/upload" element={<UploadPage />} />
-            <Route
-              path="/admin/user-management"
-              element={<UserManagementPage />}
-            />
-          </Route>
-          <Route path="/sign-in" element={<SignIn />} />
-        </Routes>
-      </Router>
-    </>
-  );
+    Axios.defaults.withCredentials = true;
+    //Defines the paths of each page
+    //This file should only have the topbar and sidebar
+    //   test
+    return (
+        <>
+            <Router>
+                <Topbar />
+                <Sidebar />
+                <Routes>
+                    <Route element={<ProtectedRoutes />}>
+                        <Route path="/reports" element={<Reports />} />
+                        <Route
+                            path="/billvalidation"
+                            element={<BillValidation />}
+                        />
+                        <Route
+                            path="/costforecast"
+                            element={<CostForecast />}
+                        />
+                        <Route path="/" element={<Dashboard />} />
+                    </Route>
+                    {/* admin routes */}
+                    <Route element={<AdminRoutes />}>
+                        <Route path="/admin/upload" element={<UploadPage />} />
+                        <Route
+                            path="/admin/user-management"
+                            element={<UserManagementPage />}
+                        />
+                    </Route>
+                    <Route path="/sign-in" element={<SignIn />} />
+                </Routes>
+            </Router>
+        </>
+    );
 };
 
 export default App;
