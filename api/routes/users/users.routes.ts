d4e1359b608e9f48361b510fc4869d1b407731c3
c2035950 <miaham@cardiff.ac.uk>
2022-12-09 15:05:26 +0000

    Add API route for user delete

--- api/routes/users/users.routes.ts
+++ api/routes/users/users.routes.ts
@@ -1,18 +1,24 @@
 import express from "express";
 import { UserManagementController } from "../../controllers/userManagement.controller";
+import { UserController } from "../../controllers/users.controller";
 
 const router = express.Router();
 
 // controller
 const userManagementController = new UserManagementController();
+const userController = new UserController();
 
 // routes
 router.get("/api/user-managements", async (req, res) => {
-  userManagementController.getAllUserManagements(req, res);
+    userManagementController.getAllUserManagements(req, res);
 });
 
 router.get("/api/user-management/:id", async (req, res) => {
-  userManagementController.findUserManagementByUserId(req, res);
+    userManagementController.findUserManagementByUserId(req, res);
+});
+
+router.delete("/api/user/:id", async (req, res) => {
+    userController.deleteUser(req, res);
 });
 
 export default router;
