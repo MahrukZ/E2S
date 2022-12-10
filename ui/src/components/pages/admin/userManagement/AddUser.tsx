31a1878df127da01673b0de74c9daba1f8cf0d26
c2035950 <miaham@cardiff.ac.uk>
2022-12-10 01:02:53 +0000

    Add functionality for adding users.

---
+++ ui/src/components/pages/admin/userManagement/AddUser.tsx
@@ -0,0 +1,198 @@
+import { useState } from "react";
+import { Button, Col, Form, Modal, Row } from "react-bootstrap";
+import { FaPlus } from "react-icons/fa";
+import { UsersService } from "../../../../services/users.service";
+import { IUser } from "./UserTable";
+
+function AddUser() {
+    const [show, setShow] = useState(false);
+    const [user, setUser] = useState<IUser>({
+        userId: 0,
+        firstName: "",
+        lastName: "",
+        email: "",
+        role: "",
+        password: "",
+        orgId: 0,
+    });
+
+    const [email, setEmail] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [org, setOrg] = useState("");
+    const [role, setRole] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleShow = () => setShow(true);
+    const handleClose = () => setShow(false);
+
+    const usersService = new UsersService();
+
+    const handleCreateUser = async () => {
+        try {
+            setUser({
+                email,
+                firstName,
+                lastName,
+                role,
+                password,
+                orgId: 1
+            });
+            await usersService.createUser(user);
+            setShow(false);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+    return (
+        <>
+            <Button variant="outline-success" onClick={handleShow}>
+                Add User <FaPlus />
+            </Button>
+
+            <Modal
+                show={show}
+                onHide={handleClose}
+                keyboard={false}
+                backdrop="static"
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Add User</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="email">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control
+                                type="email"
+                                placeholder="e.g. john_doe@example.com"
+                                autoFocus
+                                onChange={(e: any) => {
+                                    setEmail(e.target.value);
+                                }}
+                            />
+                        </Form.Group>
+
+                        <Row>
+                            <Col>
+                                <Form.Group
+                                    className="mb-3"
+                                    controlId="firstName"
+                                >
+                                    <Form.Label>First Name</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="e.g. John"
+                                        autoFocus
+                                        onChange={(e: any) => {
+                                            setFirstName(e.target.value);
+                                        }}
+                                    />
+                                </Form.Group>
+                            </Col>
+
+                            <Col>
+                                <Form.Group
+                                    className="mb-3"
+                                    controlId="lastName"
+                                >
+                                    <Form.Label>Last Name</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="e.g. Doe"
+                                        autoFocus
+                                        onChange={(e: any) => {
+                                            setLastName(e.target.value);
+                                        }}
+                                    />
+                                </Form.Group>
+                            </Col>
+                        </Row>
+
+                        <Row>
+                            <Col>
+                                <Form.Group className="mb-3" controlId="org">
+                                    <Form.Label>Organisation</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="e.g. Cardiff University"
+                                        autoFocus
+                                        onChange={(e: any) => {
+                                            setOrg(e.target.value);
+                                        }}
+                                    />
+                                </Form.Group>
+                            </Col>
+
+                            <Col>
+                                <Form.Label>Role</Form.Label>
+                                <Form.Select
+                                    className="mb-3"
+                                    onChange={(e: any) => {
+                                        setRole(e.target.value);
+                                    }}
+                                >
+                                    <option>Select a Role</option>
+                                    <option value="director of estates">
+                                        Director of Estates
+                                    </option>
+                                    <option value="facility energy manager">
+                                        Facility Energy Manager
+                                    </option>
+                                    <option value="administrator">
+                                        Administrator
+                                    </option>
+                                </Form.Select>
+                            </Col>
+
+                            <Row>
+                                <Col>
+                                    <Form.Group
+                                        className="mb-3"
+                                        controlId="password"
+                                    >
+                                        <Form.Label>
+                                            Temporary Password
+                                        </Form.Label>
+                                        <Form.Control
+                                            type="password"
+                                            autoFocus
+                                        />
+                                    </Form.Group>
+                                </Col>
+
+                                <Col>
+                                    <Form.Group
+                                        className="mb-3"
+                                        controlId="confirmPassword"
+                                    >
+                                        <Form.Label>
+                                            Confirm Password
+                                        </Form.Label>
+                                        <Form.Control
+                                            type="password"
+                                            autoFocus
+                                            onChange={(e: any) => {
+                                                setPassword(e.target.value);
+                                            }}
+                                        />
+                                    </Form.Group>
+                                </Col>
+                            </Row>
+                        </Row>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="outline-secondary" onClick={handleClose}>
+                        Cancel
+                    </Button>
+                    <Button variant="success" onClick={handleCreateUser}>
+                        Add
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+}
+
+export default AddUser;
--- ui/src/components/pages/admin/userManagement/UserManagementPage.tsx
+++ ui/src/components/pages/admin/userManagement/UserManagementPage.tsx
@@ -1,9 +1,11 @@
+import AddUser from "./AddUser";
 import UserTable from "./UserTable";
 
 function UserManagementPage() {
   return (
     <div className="text-center">
       <h1>User Management</h1>
+      <AddUser />
       <UserTable />
     </div>
   );
