6b1f13a1b13d126adcf1d4c30ebe2c6641b24dec
c2035950 <miaham@cardiff.ac.uk>
2022-12-12 14:04:31 +0000

    Add MVC layers for create sites and users

--- api/data/repositories/sitesAndUsers.repository.ts
+++ api/data/repositories/sitesAndUsers.repository.ts
@@ -2,43 +2,53 @@ import { connect } from "../config/db.config";
 import { SitesAndUsers, ISitesAndUser } from "../models/sitesAndUsers.model";
 
 export class SitesAndUsersRepository {
-  private db: any = {};
-  private sitesAndUsersRepository: any;
+    private db: any = {};
+    private sitesAndUsersRepository: any;
 
-  constructor() {
-    this.db = connect();
-    this.db.Sequelize.sync({})
-      .then(() => {
-        console.log("Sync db.");
-      })
-      .catch((err: { message: string }) => {
-        console.log("Failed to sync db: " + err.message);
-      });
-    this.sitesAndUsersRepository =
-      this.db.Sequelize.getRepository(SitesAndUsers);
-  }
-
-  async getAllSitesAndUsers(): Promise<ISitesAndUser[]> {
-    let data = [];
-    try {
-      data = await this.sitesAndUsersRepository.findAll();
-    } catch (err) {
-      throw new Error("Failed to get sites and users." || err);
+    constructor() {
+        this.db = connect();
+        this.db.Sequelize.sync({})
+            .then(() => {
+                console.log("Sync db.");
+            })
+            .catch((err: { message: string }) => {
+                console.log("Failed to sync db: " + err.message);
+            });
+        this.sitesAndUsersRepository =
+            this.db.Sequelize.getRepository(SitesAndUsers);
     }
-    return data;
-  }
 
-  async findSitesAndUsersByUserId(userId: number): Promise<ISitesAndUser[]> {
-    let data = [];
-    try {
-      data = await this.sitesAndUsersRepository.findAll({
-        where: {
-          user_id: userId,
-        },
-      });
-    } catch (err) {
-      throw new Error("Failed to get sites and users." || err);
+    async getAllSitesAndUsers(): Promise<ISitesAndUser[]> {
+        let data = [];
+        try {
+            data = await this.sitesAndUsersRepository.findAll();
+        } catch (err) {
+            throw new Error("Failed to get sites and users." || err);
+        }
+        return data;
     }
-    return data;
-  }
+
+    async findSitesAndUsersByUserId(userId: number): Promise<ISitesAndUser[]> {
+        let data = [];
+        try {
+            data = await this.sitesAndUsersRepository.findAll({
+                where: {
+                    user_id: userId,
+                },
+            });
+        } catch (err) {
+            throw new Error("Failed to get sites and users." || err);
+        }
+        return data;
+    }
+
+    async createSitesAndUsers(sitesAndUsers: ISitesAndUser): Promise<ISitesAndUser[]> {
+        let data = [];
+        try {
+            data = await this.sitesAndUsersRepository.create(sitesAndUsers);
+        } catch (err) {
+            throw new Error("Failed to create sites and users." || err);
+        }
+        return data;
+    }
 }
--- api/services/sitesAndUsers.service.ts
+++ api/services/sitesAndUsers.service.ts
@@ -1,17 +1,26 @@
+import { ISitesAndUser } from "../data/models/sitesAndUsers.model";
 import { SitesAndUsersRepository } from "../data/repositories/sitesAndUsers.repository";
 
 export class SitesAndUsersService {
-  private sitesAndUsersRepository: SitesAndUsersRepository;
+    private sitesAndUsersRepository: SitesAndUsersRepository;
 
-  constructor() {
-    this.sitesAndUsersRepository = new SitesAndUsersRepository();
-  }
+    constructor() {
+        this.sitesAndUsersRepository = new SitesAndUsersRepository();
+    }
 
-  async getAllSitesAndUsers() {
-    return await this.sitesAndUsersRepository.getAllSitesAndUsers();
-  }
+    async getAllSitesAndUsers() {
+        return await this.sitesAndUsersRepository.getAllSitesAndUsers();
+    }
 
-  async findSitesAndUsersByUserId(userId: number) {
-    return await this.sitesAndUsersRepository.findSitesAndUsersByUserId(userId);
-  }
+    async findSitesAndUsersByUserId(userId: number) {
+        return await this.sitesAndUsersRepository.findSitesAndUsersByUserId(
+            userId
+        );
+    }
+
+    async createSitesAndUsers(sitesAndUsers: ISitesAndUser) {
+        return await this.sitesAndUsersRepository.createSitesAndUsers(
+            sitesAndUsers
+        );
+    }
 }
